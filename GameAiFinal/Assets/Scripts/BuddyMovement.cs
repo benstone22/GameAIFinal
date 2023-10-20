850bbc8272fa01e5605a98a0f5098b5b4911dc12
Ben Stone <ben@lmstone.com>
2023-10-20 14:18:04 -0400

    Player Movement, Started Buddy

---
+++ GameAiFinal/Assets/Scripts/BuddyMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyMovement : MonoBehaviour
+{
+    public bool shouldFollow;
+    Rigidbody rb;
+    public Transform target;
+    public bool shouldPuzzle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
---
+++ GameAiFinal/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//In memory of doug tillman
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private BoxCollider boxCollider;
+    public Vector3 moveForce;
+    public float speed;
+    public float jumpForce;
+    private bool canJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        boxCollider = GetComponent<BoxCollider>();
+        speed = 25f;
+        jumpForce = 1000f;
+        canJump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement
+        float inpX = Input.GetAxis("Horizontal")*speed;
+        moveForce.x = inpX;
+        float inpZ = Input.GetAxis("Vertical")*speed;
+        moveForce.z = inpZ;
+        rb.AddForce(moveForce);
+        //canJump = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down),1f);
+        
+        //jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           jump(gameObject,rb,jumpForce);
+        }
+
+        
+        
+        
+    }
+    public void jump(GameObject gameObj, Rigidbody rb, float jumpForce)
+    {
+        
+        canJump = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 10f);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down), Color.red, 10f);
+        if (canJump)
+        {
+                
+            Vector3 jump = new Vector3(0, jumpForce, 0);
+            rb.AddForce(jump);
+            canJump = false;
+        }
+
+    }
+    
+    
+}
