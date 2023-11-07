9424d8638946f0ce8d9fdc2ad118b4923004ef32
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-11-07 14:25:57 -0500

    Started Spacial Quantization for Astar

---
+++ GameAiFinal/Assets/Scripts/Spacial Quatization.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacialQuatization : MonoBehaviour
+{
+    private Dictionary<Vector3Int, HashSet<GameObject>> hashmap;
+    private Dictionary<Vector3Int, Vector3Int> cameFrom;  //flowfield
+    public static Vector3Int Quantize(Vector3 v, float resolution = 1f)
+    {
+        return new Vector3Int(Mathf.FloorToInt(v.x / resolution), Mathf.FloorToInt(v.y /resolution), Mathf.FloorToInt(v.z / resolution));
+    }
+
+    public Vector3 dequantize(Vector3Int index, float resolution = 1f)
+    {
+        return new Vector3((float)index.x * resolution + resolution / 2.0f, (float)index.y * resolution + resolution / 2.0f);
+    }
+
+    public void Move(GameObject go, Vector3 previous, Vector3 current)
+    {
+        Vector3Int previousBucket = Quantize(previous);
+        Vector3Int currentBucket = Quantize(current);
+
+        if (previousBucket != currentBucket) 
+        {
+            hashmap[previousBucket].Remove(go);
+            hashmap[currentBucket].Add(go);
+        }
+
+        //recalc camefrom map 
+    }
+
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
