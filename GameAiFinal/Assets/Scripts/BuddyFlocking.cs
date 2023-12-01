1a57fc714286e4323063177984bcdd5e3a56ea2b
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-12-01 14:28:54 -0500

    Started Fixing Flocking
    Did some Calculation Changes.

--- GameAiFinal/Assets/Scripts/BuddyFlocking.cs
+++ GameAiFinal/Assets/Scripts/BuddyFlocking.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 public class BuddyFlocking : MonoBehaviour
@@ -10,17 +11,16 @@ public class BuddyFlocking : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody rb;
     public GameObject player;
-    private Vector3 cohesionForce;
-    private Vector3 allignmentForce;
-    private Vector3 separationForce;
-    
-    private 
+    private Vector3 cohesionForce = Vector2.zero;
+    private Vector3 allignmentForce = Vector2.zero;
+    private Vector3 separationForce = Vector2.zero;
+    private Vector3 flockingForce = Vector2.zero;
+
+    [FormerlySerializedAs("PlayerNeighborhood")] public List<GameObject> neighborhood;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        cohesionForce = Vector2.zero;
-        allignmentForce = Vector2.zero;
-        separationForce = Vector2.zero;
+        neighborhood.Add(player);
     }
 
     // Update is called once per frame
@@ -30,21 +30,39 @@ public class BuddyFlocking : MonoBehaviour
         CohesionCalc(col);
         AllignmentCalc(col);
         SeparationCalc(col);
+
+        flockingForce = cohesionForce + separationForce + allignmentForce;
         
         rb.AddForce(cohesionForce + separationForce + allignmentForce);
     }
     
     private void CohesionCalc(Collider col)
     {
+        Vector3 posSum = Vector3.zero;
+        Vector3 PosCenter = Vector3.zero;
         
-        cohesionForce = col.transform.position - gameObject.transform.position;
-        cohesionForce = cohesionForce.normalized;
+        if (neighborhood.Count != 0)
+        {
+            for (int i = 0; i < neighborhood.Count; ++i)
+            {
+                posSum += neighborhood[i].transform.position;
+            }
+            PosCenter = posSum / (neighborhood.Count);
+
+            Vector3 distVect = PosCenter - this.transform.position;
+
+            cohesionForce = distVect.normalized;
+        }
+        // cohesionForce = col.transform.position - gameObject.transform.position;
+        // cohesionForce = cohesionForce.normalized;
         
         
     }
     private void AllignmentCalc(Collider col)
     {
-        allignmentForce= player.GetComponent<Rigidbody>().velocity;
+        allignmentForce += player.GetComponent<Rigidbody>().velocity;
+        allignmentForce /= neighborhood.Count + 1;
+        allignmentForce = allignmentForce.normalized;
     }
     private void SeparationCalc(Collider col)
     {
